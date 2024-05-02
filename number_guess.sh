a44a8d4aced4684a74c2f1728470893a1a91b336
SelenaCua8 Cuadra <selenac99@hotmail.com>
2024-05-02 12:15:42 +0000

    refactor: add validate userName

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,9 @@ PSQL="psql --username=freecodecamp --dbname=number_guess --no-align --tuples-onl
 
 echo "Enter your username:"
 read USERNAME
+
+
+
+USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT  COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
