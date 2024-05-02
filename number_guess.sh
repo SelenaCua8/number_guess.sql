3ae6bf1c5c368020d20366769ddc65e8f32335d7
SelenaCua8 Cuadra <selenac99@hotmail.com>
2024-05-02 12:22:12 +0000

    refactor: add validate userName

--- number_guess.sh
+++ number_guess.sh
@@ -10,3 +10,12 @@ read USERNAME
 USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
 GAMES_PLAYED=$($PSQL "SELECT  COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
 BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+
+
+if [[ -z $USERNAME_AVAIL ]]
+then
+  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')" )
+  echo  "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
